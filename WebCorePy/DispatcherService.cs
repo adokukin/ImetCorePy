5b2c57af542a2e7914ef774c89fa3873dec77915
Alexander Dokukin <dokukin.it@gmail.com>
2025-06-02 15:24:28 +0300

    dispatcher: empty class prepared

---
+++ WebCorePy/DispatcherService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebCorePy
+{
+    public class DispatcherService : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
