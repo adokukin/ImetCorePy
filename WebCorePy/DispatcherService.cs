f89f4b451fc5b45ef3fe9ff4e52ae4521b57d5d0
Alexander Dokukin <dokukin.it@gmail.com>
2025-10-24 14:15:04 +0300

    dispatcher: processor task extracted to a separate file

--- WebCorePy/DispatcherService.cs
+++ WebCorePy/DispatcherService.cs
@@ -7,32 +7,6 @@ using System.Threading.Tasks;
 
 namespace WebCorePy
 {
-    public enum State
-    {
-        EMPTY,
-        IN_PROGRESS,
-        READY
-    }
-
-    public struct Processor
-    {
-        public string process; // TODO: proper type
-        public State state;
-        public DateTime? start;
-        public DateTime? end;
-        public string file_train;
-        public string file_test;
-        public Processor ()
-        {
-            process = null;
-            state = State.EMPTY;
-            start = null;
-            end = null;
-            file_train = null;
-            file_test = null;
-        }
-    }
-
     public class DispatcherService : BackgroundService
     {
         public const int NumProcessors = 5;
---
+++ WebCorePy/Processor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WebCorePy
+{
+    public enum State
+    {
+        EMPTY,
+        IN_PROGRESS,
+        READY
+    }
+
+    public struct Processor
+    {
+        public string process; // TODO: proper type
+        public State state;
+        public DateTime? start;
+        public DateTime? end;
+        public string file_train;
+        public string file_test;
+        public Processor()
+        {
+            process = null;
+            state = State.EMPTY;
+            start = null;
+            end = null;
+            file_train = null;
+            file_test = null;
+        }
+    }
+}
