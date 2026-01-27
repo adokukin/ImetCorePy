2ff740753c27f80936b9030bd8cf94ae8f2b4bec
Alexander Dokukin <dokukin.it@gmail.com>
2026-01-27 18:57:24 +0300

    settings: cookies set interactively TODO: read on initialization and remove server-side processing

--- WebCorePy/wwwroot/js/site.js
+++ WebCorePy/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+window.SetCookie = function (name, value, days=365) {
+    var expires = ""
+    if (days) {
+        var date = new Date()
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000))
+        expires = "; expires=" + date.toGMTString()
+    }
+    document.cookie = name + "=" + value + expires + "; path=/"
+}
+
+window.GetCookies = function () {
+    return document.cookie
+}
