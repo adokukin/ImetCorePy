af93c9615c38564fb3e5b6f3b6e523acd2d716c1
Alexander Dokukin <dokukin.it@gmail.com>
2026-03-06 19:38:57 +0300

    dummy: random output skipping removed

--- WebCorePy/py/dummy.py
+++ WebCorePy/py/dummy.py
@@ -3,7 +3,7 @@ import random
 import sys
 
 def eprint(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
+    print(*args, file=sys.stderr, **kwargs, flush=True)
 
 def report_progress(progress):
     # TODO: decorate additionally if methods use stderr too
@@ -13,7 +13,6 @@ TOTAL = 10
 for i in range(TOTAL):
     report_progress(i / TOTAL)
     time.sleep(.5)
-    if random.random() > .5:
-        print('Method output: {} s remaining'.format(TOTAL - i))
+    print('Method output: {} s remaining'.format(TOTAL - i), flush=True)
     time.sleep(.5)
 report_progress(1)
