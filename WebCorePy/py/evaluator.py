c0f7354f8b7c92615f0b3cfb1bb4042db491f4f4
Alexander Dokukin <dokukin.it@gmail.com>
2026-04-09 17:10:11 +0300

    evaluator: basic training implemented TODO: add predicting

--- WebCorePy/py/evaluator.py
+++ WebCorePy/py/evaluator.py
@@ -3,12 +3,15 @@ import random
 import sys
 import argparse
 import json
+from datetime import datetime
 
 from contextlib import redirect_stderr
+from importlib import import_module
 
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import LeaveOneOut, KFold
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs, flush=True)
@@ -22,20 +25,20 @@ def report_results(results):
     eprint(json.dumps(message))
 
 def load_sample(filename):
-  df = pd.read_excel(filename, index_col=0, header=0)
+    df = pd.read_excel(filename, index_col=0, header=0)
   
-  target_idx = df.columns[0]
-  features = df.drop([target_idx], axis=1)
-  target = df[target_idx]
+    target_idx = df.columns[0]
+    features = df.drop([target_idx], axis=1)
+    target = df[target_idx]
 
-  return features.values, target.values
+    return features.values, target.values
 
 def get_validator(folds = 0):
-  if folds == 0:
-    validator = LeaveOneOut()
-  else:
-    validator = KFold(n_splits = folds, shuffle = True) # TODO: random_state
-  return validator
+    if folds == 0:
+        validator = LeaveOneOut()
+    else:
+        validator = KFold(n_splits = folds, shuffle = True) # TODO: random_state
+    return validator
 
 def validated_predict(model, X, y, folds=0):
     validator = get_validator(folds)
@@ -43,19 +46,26 @@ def validated_predict(model, X, y, folds=0):
     steps = X.shape[0] if folds==0 else folds
 
     for step, (train_index, test_index) in enumerate(validator.split(X)):
-        # TODO: print step info
+        print('Training step {} with {} objects'.format(step + 1, train_index.shape[0]), flush=True)
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
     
-        model.fit(X_train, y_train)
-        result[test_index] = model.predict(X_test)
+        with redirect_stderr(sys.stdout):
+            model.fit(X_train, y_train)
+            result[test_index] = model.predict(X_test)
 
         report_progress(round((step + 1) / steps, 2))
 
     return result
 
 def get_model(algorithm):
-    pass
+    package_name = algorithm["package"]
+    method_name = algorithm["name"]
+    parameters = algorithm["settings"]
+
+    module = import_module(package_name)
+    model = getattr(module, method_name)(**parameters)
+    return model
 
 parser = argparse.ArgumentParser(prog='evaluator')
 parser.add_argument('-a', '--algorithm', help='JSON parameters of an algorithm')
@@ -63,14 +73,41 @@ parser.add_argument('-f', '--folds', type=int, help='number of validation folds'
 parser.add_argument('-d', '--data', help='dataset filename')
 args = parser.parse_args()
 
-# TODO: is parameters check needed here?
-algorithm = json.loads(args.algorithm)
-validator = LeaveOneOut() if args.folds == 0 else KFold(args.folds)
+try:
+    algorithm = json.loads(args.algorithm)
+    # TODO: log with timestamps
+    print('Validating method {}'.format(algorithm['name']), flush=True)
 
-TOTAL = 10
-for i in range(TOTAL):
-    report_progress(i / TOTAL)
-    time.sleep(.5)
-    print('Method output: {} s remaining ({})'.format(TOTAL - i, algorithm['name']), flush=True)
-    time.sleep(.5)
+    validator = LeaveOneOut() if args.folds == 0 else KFold(args.folds)
+
+    model = get_model(algorithm)
+    X, y = load_sample(args.data)
+    print('Sample \'{}\' loaded'.format(args.data), flush=True)
+
+    start = datetime.now()
+    predicted = validated_predict(model, X, y, args.folds)
+
+    results = {
+        "folds": args.folds,
+        "method": algorithm["name"],
+        "r2": r2_score(y, predicted),
+        "mae": mean_absolute_error(y, predicted),
+        "mse": mean_squared_error(y, predicted),
+        "time": (datetime.now() - start).total_seconds(),
+        "status": "ok"
+    }
+except Exception as e:
+    results = {
+        "folds": args.folds,
+        "method": algorithm["name"],
+        "r2": None,
+        "mae": None,
+        "mse": None,
+        "time": (datetime.now() - start).total_seconds() if 'start' in locals() else None,
+        "status": str(e)
+    }
+
+print(results, flush=True)
+report_results(results)
+
 report_progress(1)
